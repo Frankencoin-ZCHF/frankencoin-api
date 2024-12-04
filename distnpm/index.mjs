5ac10e85332d051f7b1e2313e479f4cca401e9ea
samclassix <96245183+samclassix@users.noreply.github.com>
2024-12-04 18:01:21 +0100

    Mintinghub v2 (#20)
    * feat: Leadrate and minor improvements
    
    * feat: new test deployment addresses, rebase from main, rm dir .dev
    
    * feat: polling delay config, leadrate implementation, telegram alert, position V2 merge
    
    * feat: flex api config, clean up
    
    * feat: integration of @frankencoin/zchf package
    
    * feat: transpiled as bundle/module, @frankencoin/zchf 0.2.3
    
    * feat: challenges and bids v2
    
    * fix: fetch checks and telegram bot bid taken message
    
    * fix: fetch verify data
    
    * feat: updated zchf v0.2.9, minor fixes in decimals
    
    * feat: update to zchf v0.2.10
    
    * feat: integration of saving and user table
    
    * feat: npmjs v0.2.2
    
    * fix: npmjs 0.2.3, error in module resolution
    
    * feat: @frankencoin/zchf 0.2.11
    
    * fix: wrong vars, typo
    
    * feat: zchf 0.2.12
    
    * feat: zchf 0.2.13
    
    * feat: zchf 0.2.14
    
    * feat: zchf 0.2.15
    
    * fix: leadrate isPending
    
    * feat: flex entries for all user
    
    * feat: extend analytics with interest costs
    
    * feat: new test deployment with mainnet code
    
    * feat: transforming into true NPM Package/Module, fix position minting restrictions and values
    
    * feat: limitForPosition in V2, limited my collateral and price
    
    * feat: npm pkg build and published

---
+++ distnpm/index.mjs
@@ -0,0 +1,15 @@
+// challenges/challenges.types.ts
+var ChallengesQueryStatus = /* @__PURE__ */ ((ChallengesQueryStatus2) => {
+  ChallengesQueryStatus2["Active"] = "Active";
+  ChallengesQueryStatus2["Success"] = "Success";
+  return ChallengesQueryStatus2;
+})(ChallengesQueryStatus || {});
+var BidsQueryType = /* @__PURE__ */ ((BidsQueryType2) => {
+  BidsQueryType2["Averted"] = "Averted";
+  BidsQueryType2["Succeeded"] = "Succeeded";
+  return BidsQueryType2;
+})(BidsQueryType || {});
+export {
+  BidsQueryType,
+  ChallengesQueryStatus
+};
