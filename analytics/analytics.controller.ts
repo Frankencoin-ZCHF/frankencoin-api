0d089a55790ada53cc566090155687bbedca9527
samclassix <samclassix@proton.me>
2025-01-02 17:47:06 +0100

    feat: added endpoint with formatUnits 1e18

--- analytics/analytics.controller.ts
+++ analytics/analytics.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AnalyticsService } from './analytics.service';
+import { formatUnits } from 'viem';
 
 @ApiTags('Analytics Controller')
 @Controller('analytics')
@@ -116,6 +117,95 @@ export class AnalyticsController {
 				e.netRealized365EarningsToEquityRatio,
 				e.netRealized365EarningsPerToken,
 				e.netRealized365EarningsPerTokenYield,
+			];
+			csv += toStrore.join(', ') + ' \n';
+		}
+
+		if (pageInfo == 'true') {
+			csv += `${JSON.stringify(data.pageInfo)}`;
+		}
+
+		return csv;
+	}
+
+	@Get('transactionLog/csv1e18')
+	@ApiResponse({
+		description: 'Returns a transaction log in CSV format, 1e18 adjusted values, default: latest on line: 1',
+	})
+	@ApiQuery({
+		name: 'pageInfo',
+		required: false,
+		type: Boolean,
+	})
+	@ApiQuery({
+		name: 'firstItem',
+		required: false,
+		type: Boolean,
+	})
+	@ApiQuery({
+		name: 'limit',
+		required: false,
+		type: Number,
+	})
+	@ApiQuery({
+		name: 'after',
+		required: false,
+		type: String,
+	})
+	async getTransactionLogCsv1e18(
+		@Query('pageInfo') pageInfo?: string,
+		@Query('firstItem') firstItem?: string,
+		@Query('limit') limit?: number,
+		@Query('after') after?: string
+	) {
+		const data = await this.analytics.getTransactionLog(firstItem == 'true' ? false : true, limit ?? 50, after ?? '');
+		const header = data.logs.length > 0 ? Object.keys(data.logs[0]).slice(1, -1).join(', ') + ' \n' : '';
+		let csv: string = header ?? '';
+
+		for (const e of data.logs) {
+			const toStrore = [
+				e.timestamp,
+				e.kind,
+				formatUnits(e.amount, 18),
+
+				formatUnits(e.totalInflow, 18),
+				formatUnits(e.totalOutflow, 18),
+				formatUnits(e.totalTradeFee, 18),
+
+				formatUnits(e.totalSupply, 18),
+				formatUnits(e.totalEquity, 18),
+				formatUnits(e.equityToSupplyRatio, 18),
+				formatUnits(e.totalSavings, 18),
+				formatUnits(e.savingsToSupplyRatio, 18),
+
+				formatUnits(e.fpsTotalSupply, 18),
+				formatUnits(e.fpsPrice, 18),
+
+				formatUnits(e.totalMintedV1, 18),
+				formatUnits(e.totalMintedV2, 18),
+				formatUnits(e.mintedV1ToSupplyRatio, 18),
+				formatUnits(e.mintedV2ToSupplyRatio, 18),
+
+				formatUnits(e.currentLeadRate, 18),
+				formatUnits(e.claimableInterests, 18),
+				formatUnits(e.projectedInterests, 18),
+				formatUnits(e.impliedV1Interests, 18),
+				formatUnits(e.impliedV2Interests, 18),
+
+				formatUnits(e.impliedV1AvgBorrowRate, 18),
+				formatUnits(e.impliedV2AvgBorrowRate, 18),
+
+				formatUnits(e.netImpliedEarnings, 18),
+				formatUnits(e.netImpliedEarningsToSupplyRatio, 18),
+				formatUnits(e.netImpliedEarningsToEquityRatio, 18),
+				formatUnits(e.netImpliedEarningsPerToken, 18),
+				formatUnits(e.netImpliedEarningsPerTokenYield, 18),
+
+				formatUnits(e.netRealized365Earnings, 18),
+				formatUnits(e.netRealized365EarningsToSupplyRatio, 18),
+				formatUnits(e.netRealized365EarningsToEquityRatio, 18),
+				formatUnits(e.netRealized365EarningsPerToken, 18),
+				formatUnits(e.netRealized365EarningsPerTokenYield, 18),
 			];
 			csv += toStrore.join(', ') + ' \n';
 		}
